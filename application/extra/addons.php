16c978762be2efceb5d6d7691e12f292b3ebc2b2
张金鹏 <740757510@qq.com>
2018-11-20 11:05:37 +0800

    first commit

---
+++ application/extra/addons.php
@@ -0,0 +1,31 @@
+<?php
+
+return array (
+  'autoload' => false,
+  'hooks' => 
+  array (
+    'ems_send' => 
+    array (
+      0 => 'faems',
+    ),
+    'ems_notice' => 
+    array (
+      0 => 'faems',
+    ),
+    'action_begin' => 
+    array (
+      0 => 'geetest',
+    ),
+    'config_init' => 
+    array (
+      0 => 'geetest',
+    ),
+    'admin_login_init' => 
+    array (
+      0 => 'loginbg',
+    ),
+  ),
+  'route' => 
+  array (
+  ),
+);
---
+++ application/route/test.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: administrator
+ * Date: 2018/11/20
+ * Time: 9:47
+ */
+use think\Route;
+
+Route::rule('/test','/admin/user/user/test');
